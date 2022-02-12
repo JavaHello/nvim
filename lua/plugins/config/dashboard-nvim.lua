392b4d052cec64a0eb1c5c277ef2ba5ea49914af
kaiv2 <kaiv2@outlook.com>
2022-02-12 22:19:00 +0800

    config

--- lua/plugins/config/dashboard-nvim.lua
+++ lua/plugins/config/dashboard-nvim.lua
@@ -1,3 +1,7 @@
-vim.cmd([[
-let g:dashboard_default_executive ='telescope'
-]])
+local g = vim.g
+
+g.dashboard_disable_at_vimenter = 0
+g.dashboard_disable_statusline = 1
+g.dashboard_default_executive = "telescope"
+-- g.dashboard_custom_header = {
+-- }
