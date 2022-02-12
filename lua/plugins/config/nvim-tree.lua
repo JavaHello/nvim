9110861f76e61a89f2cae0e9d03514a6f6be4858
kaiv2 <kaiv2@outlook.com>
2022-02-12 22:51:28 +0800

    config

--- lua/plugins/config/nvim-tree.lua
+++ lua/plugins/config/nvim-tree.lua
@@ -65,3 +65,20 @@ require'nvim-tree'.setup {
 }
 
 
+local g = vim.g
+
+
+g.nvim_tree_indent_markers = 1
+
+g.nvim_tree_window_picker_exclude = {
+   filetype = { "notify", "packer", "qf" },
+   buftype = { "terminal" },
+}
+
+g.nvim_tree_show_icons = {
+   folders = 1,
+   files = 1,
+   git = 1,
+}
+
+
