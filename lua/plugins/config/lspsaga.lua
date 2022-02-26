b4bb1191f7ce0d7575f8640c0e56d86b696245b7
kaiv2 <kaiv2@outlook.com>
2022-02-26 17:54:20 +0800

    update

--- lua/plugins/config/lspsaga.lua
+++ lua/plugins/config/lspsaga.lua
@@ -38,8 +38,11 @@ local saga = require 'lspsaga'
 -- server_filetype_map = {}
 
 saga.init_lsp_saga({
-    error_sign = '',
-    warn_sign = '',
-    hint_sign = '',
-    infor_sign = ''
+  error_sign = '',
+  warn_sign = '',
+  hint_sign = '',
+  infor_sign = '',
+  code_action_prompt = {
+    enable = false,
+  },
 })
