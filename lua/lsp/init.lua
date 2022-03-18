3116b04da3722914af815b51416d1a504b18a27c
kaiv2 <kaiv2@outlook.com>
2022-03-18 18:49:31 +0800

    notify conf

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -74,14 +74,13 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.s
 })
 
 -- suppress error messages from lang servers
-vim.notify = function(msg, log_level)
-  if msg:match "exit code" then
-    return
-  end
-  if log_level == vim.log.levels.ERROR then
-    vim.api.nvim_err_writeln(msg)
-  else
-    vim.api.nvim_echo({ { msg } }, true, {})
-  end
-end
-
+-- vim.notify = function(msg, log_level)
+--   if msg:match "exit code" then
+--     return
+--   end
+--   if log_level == vim.log.levels.ERROR then
+--     vim.api.nvim_err_writeln(msg)
+--   else
+--     vim.api.nvim_echo({ { msg } }, true, {})
+--   end
+-- end
