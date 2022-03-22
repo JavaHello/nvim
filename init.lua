237b83df59ab95bffe4bffc9e42e7122237a5b31
kaiv2 <kaiv2@outlook.com>
2022-03-22 15:03:25 +0800

    update

--- init.lua
+++ init.lua
@@ -4,5 +4,9 @@ require('lsp')
 require('keybindings')
 
 -- vim.api.nvim_command('colorscheme gruvbox')
--- vim.cmd("highlight Normal guibg=NONE ctermbg=None")
-vim.cmd("colorscheme gruvbox")
+vim.cmd[[ 
+colorscheme gruvbox
+set background=dark
+"  丢失配色, 变为透明
+highlight Normal guibg=NONE ctermbg=None
+]]
