130263d52873da208f6e6725f9634b8ba9e3034d
kaiv2 <kaiv2@outlook.com>
2022-03-28 10:51:00 +0800

    update

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -80,6 +80,19 @@ vim.cmd([[
 nnoremap <silent> <space>dr :lua require("dapui").float_element(vim.Nil, { enter = true}) <CR>
 ]])
 
+-- bufferline.nvim
+vim.cmd[[
+nnoremap <silent><leader>1 <Cmd>BufferLineGoToBuffer 1<CR>
+nnoremap <silent><leader>2 <Cmd>BufferLineGoToBuffer 2<CR>
+nnoremap <silent><leader>3 <Cmd>BufferLineGoToBuffer 3<CR>
+nnoremap <silent><leader>4 <Cmd>BufferLineGoToBuffer 4<CR>
+nnoremap <silent><leader>5 <Cmd>BufferLineGoToBuffer 5<CR>
+nnoremap <silent><leader>6 <Cmd>BufferLineGoToBuffer 6<CR>
+nnoremap <silent><leader>7 <Cmd>BufferLineGoToBuffer 7<CR>
+nnoremap <silent><leader>8 <Cmd>BufferLineGoToBuffer 8<CR>
+nnoremap <silent><leader>9 <Cmd>BufferLineGoToBuffer 9<CR>
+]]
+
 local pluginKeys = {}
 
 -- lsp 回调函数快捷键设置
