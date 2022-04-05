b5d45dae05019b2fe8622a20e9687501487713be
kaiv2 <kaiv2@outlook.com>
2022-04-05 15:50:47 +0800

    nvim-tree auto close feature has been removed

--- lua/plugins/config/nvim-tree.lua
+++ lua/plugins/config/nvim-tree.lua
@@ -3,7 +3,7 @@ require'nvim-tree'.setup {
   hijack_netrw        = true,
   open_on_setup       = false,
   ignore_ft_on_setup  = { "dashboard" },
-  auto_close          = true,
+  -- auto_close          = true,
   auto_reload_on_write = true,
   open_on_tab         = false,
   hijack_cursor       = false,
