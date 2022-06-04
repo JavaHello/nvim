4664d3beec33535e0d2815ee1b05b696834fc28f
luokai <kaiv2@outlook.com>
2022-06-04 15:18:17 +0800

    限定支持高亮的语言

--- lua/plugins/config/nvim-treesitter.lua
+++ lua/plugins/config/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-  ensure_installed = "all",
+  ensure_installed = {"lua", "java", "javascript", "html", "css", "c", "cpp", "go", "rust", "python"},
 
   -- Install languages synchronously (only applied to `ensure_installed`)
   sync_install = false,
