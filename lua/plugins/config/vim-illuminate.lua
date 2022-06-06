0dd43bdbf70e0d62ee5924ab1200850b7a54a11d
luokai <kaiv2@outlook.com>
2022-06-06 13:52:55 +0800

    update

--- lua/plugins/config/vim-illuminate.lua
+++ lua/plugins/config/vim-illuminate.lua
@@ -1,7 +1,6 @@
 -- https://github.com/RRethy/vim-illuminate
 -- 禁止高亮的文件类型
 vim.g.Illuminate_ftblacklist = {
-    "NvimTree",
-    "vista_kind",
-    "alpha"
+  "vista_kind",
+  "help", "terminal", "packer", "markdown", "git", "text", "NvimTree", "dashboard", "alpha"
 }
