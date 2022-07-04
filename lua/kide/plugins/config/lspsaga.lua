4c6818c8642c503edb48b362f0fbecc52331df0f
luokai <kaiv2@outlook.com>
2022-07-04 23:05:16 +0800

    lspsaga

--- lua/kide/plugins/config/lspsaga.lua
+++ lua/kide/plugins/config/lspsaga.lua
@@ -37,11 +37,8 @@ local saga = require("lspsaga")
 -- server_filetype_map = {}
 
 saga.init_lsp_saga({
-	error_sign = "",
-	warn_sign = "",
-	hint_sign = "",
-	infor_sign = "",
-	code_action_prompt = {
-		enable = false,
+	diagnostic_header = { " ", " ", " ", " " },
+	server_filetype_map = {
+		metals = { "java" },
 	},
 })
