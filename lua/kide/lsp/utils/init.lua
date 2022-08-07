1722d1c56acbbc2a549a832908c82472e219e5df
luokai <kaiv2@outlook.com>
2022-08-07 16:26:41 +0800

    clang_format

--- lua/kide/lsp/utils/init.lua
+++ lua/kide/lsp/utils/init.lua
@@ -56,6 +56,10 @@ format_lsp_mapping["http"] = "null-ls"
 -- gofmt
 format_lsp_mapping["go"] = "null-ls"
 
+-- clang_format
+format_lsp_mapping["c"] = "null-ls"
+format_lsp_mapping["cpp"] = "null-ls"
+
 M.filter_format_lsp_client = function(client, bufnr)
   local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
   local cn = format_lsp_mapping[filetype]
