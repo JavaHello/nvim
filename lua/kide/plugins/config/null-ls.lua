ed8ef348321684b487ec93ca8af830e4a3171c07
luokai <kaiv2@outlook.com>
2022-08-20 23:59:41 +0800

    fmt

--- lua/kide/plugins/config/null-ls.lua
+++ lua/kide/plugins/config/null-ls.lua
@@ -50,7 +50,23 @@ local sources = {
   }),
   -- null_ls.builtins.formatting.google_java_format,
   -- null_ls.builtins.diagnostics.semgrep,
-  null_ls.builtins.formatting.rustfmt,
+  null_ls.builtins.formatting.rustfmt.with({
+    extra_args = function(params)
+      local Path = require("plenary.path")
+      local cargo_toml = Path:new(params.root .. "/" .. "Cargo.toml")
+
+      if cargo_toml:exists() and cargo_toml:is_file() then
+        for _, line in ipairs(cargo_toml:readlines()) do
+          local edition = line:match([[^edition%s*=%s*%"(%d+)%"]])
+          if edition then
+            return { "--edition=" .. edition }
+          end
+        end
+      end
+      -- default edition when we don't find `Cargo.toml` or the `edition` in it.
+      return { "--edition=2021" }
+    end,
+  }),
   -- null_ls.builtins.diagnostics.semgrep.with({
   -- 	method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
   -- 	extra_args = { "--config", "p/java" },
