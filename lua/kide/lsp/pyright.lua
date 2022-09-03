22e162c2a2ed0af9d7d4608a7fc2ae24addff5d8
luokai <kaiv2@outlook.com>
2022-09-03 19:38:32 +0800

    pyright

--- lua/kide/lsp/pyright.lua
+++ lua/kide/lsp/pyright.lua
@@ -1,16 +1,2 @@
 return {
-  server = {
-    organize_imports = function()
-      local params = {
-        command = "pyright.organizeimports",
-        arguments = { vim.uri_from_bufnr(0) },
-      }
-      vim.lsp.buf.execute_command(params)
-    end,
-    on_attach = function(_, _)
-      vim.cmd([[
-    command! -nargs=0 OR   :lua require'lsp.pyright'.organize_imports()
-  ]])
-    end,
-  },
 }
