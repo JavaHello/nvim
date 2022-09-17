e20def3a0af54ad0b0087a8a9aca88650f6c09e2
luokai <kaiv2@outlook.com>
2022-09-17 20:13:10 +0800

    默认关闭 context

--- lua/kide/plugins/config/nvim-treesitter-context.lua
+++ lua/kide/plugins/config/nvim-treesitter-context.lua
@@ -1,5 +1,5 @@
 require("treesitter-context").setup({
-  enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+  enable = false, -- Enable this plugin (Can be enabled/disabled later via commands)
   max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
   trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
   patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
