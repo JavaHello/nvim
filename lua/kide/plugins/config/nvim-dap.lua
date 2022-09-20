da14e363d4e0f5589c91d48fb8b143fc5a79b25a
luokai <kaiv2@outlook.com>
2022-09-20 14:12:07 +0800

    debug UI

--- lua/kide/plugins/config/nvim-dap.lua
+++ lua/kide/plugins/config/nvim-dap.lua
@@ -31,15 +31,15 @@ dapui.setup({
 
 dap.defaults.fallback.terminal_win_cmd = "belowright 12new | set filetype=dap-terminal"
 
-dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
-end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
-end
+-- dap.listeners.after.event_initialized["dapui_config"] = function()
+--   dapui.open()
+-- end
+-- dap.listeners.before.event_terminated["dapui_config"] = function()
+--   dapui.close()
+-- end
+-- dap.listeners.before.event_exited["dapui_config"] = function()
+--   dapui.close()
+-- end
 
 -- vim.fn.sign_define('DapBreakpoint', { text = '🔴', texthl = '', linehl = '', numhl = '' })
 vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "Debug", linehl = "", numhl = "" })
