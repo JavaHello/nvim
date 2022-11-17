b49a35512fd4bb6dfd0b4d8c78b5e96048187f21
luokai <kaiv2@outlook.com>
2022-11-17 21:16:40 +0800

    leap conf

--- lua/kide/plugins/init.lua
+++ lua/kide/plugins/init.lua
@@ -675,6 +675,9 @@ require("packer").startup({
       end,
       config = function()
         require("leap").add_default_mappings()
+        require("leap").opts.safe_labels = {}
+        vim.keymap.del({ "x", "o" }, "x")
+        vim.keymap.del({ "x", "o" }, "X")
       end,
     })
 
