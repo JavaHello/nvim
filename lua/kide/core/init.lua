68064c5ce8bace4453ca40c5d8fa397b0916a8fc
luokai <kaiv2@outlook.com>
2022-11-17 09:44:46 +0800

    maven pom.xml

--- lua/kide/core/init.lua
+++ lua/kide/core/init.lua
@@ -2,4 +2,5 @@ require("kide.core.basic")
 
 vim.schedule(function()
   require("kide.core.utils.plantuml").setup()
+  require("kide.core.utils.maven").setup()
 end)
--- lua/kide/core/utils/maven.lua
+++ lua/kide/core/utils/maven.lua
@@ -28,5 +28,24 @@ M.maven_command = function(buf, settings)
   end, {
     nargs = 0,
   })
+  vim.api.nvim_buf_create_user_command(buf, "MavenDownloadSources", function()
+    require("toggleterm").exec("mvn dependency:sources -DdownloadSources=true" .. settings_opt(settings))
+  end, {
+    nargs = 0,
+  })
+end
+
+M.setup = function()
+  local group = vim.api.nvim_create_augroup("kide_jdtls_java_maven", { clear = true })
+  vim.api.nvim_create_autocmd({ "FileType" }, {
+    group = group,
+    pattern = { "xml" },
+    desc = "maven_command",
+    callback = function(e)
+      if vim.endswith(e.file, "pom.xml") then
+        M.maven_command(e.buf)
+      end
+    end,
+  })
 end
 return M
