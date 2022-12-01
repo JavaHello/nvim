7ace430e96284a77c80c22cb7ede5bfda1917a24
luokai <kaiv2@outlook.com>
2022-12-01 22:10:40 +0800

    rust

--- lua/kide/lsp/rust_analyzer.lua
+++ lua/kide/lsp/rust_analyzer.lua
@@ -1,13 +1,54 @@
+local vscode = require("kide.core.vscode")
+local utils = require("kide.core.utils")
 -- Update this path
-local extension_path = "/Users/kailuo/.vscode/extensions/vadimcn.vscode-lldb-1.7.0/"
-local codelldb_path = extension_path .. "adapter/codelldb"
-local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+local extension_path = vscode.find_one("/vadimcn.vscode-lldb-*")
+local codelldb_path = (function()
+  if not extension_path then
+    return nil
+  end
+  if utils.is_win then
+    return extension_path .. "adapter\\codelldb.exe"
+  else
+    return extension_path .. "adapter/codelldb"
+  end
+end)()
+local liblldb_path = (function()
+  if not extension_path then
+    return nil
+  end
+  if utils.is_mac then
+    return extension_path .. "lldb/lib/liblldb.dylib"
+  elseif utils.is_win then
+    return extension_path .. "lldb\\lib\\liblldb.dll"
+  else
+    return extension_path .. "lldb/lib/liblldb.so"
+  end
+end)()
 
+local adapter = function()
+  if codelldb_path and liblldb_path then
+    print(codelldb_path)
+    return require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
+  end
+end
+local rt = require("rust-tools")
 return {
   dap = {
-    adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+    adapter = adapter(),
+  },
+  tools = {
+    inlay_hints = {
+      parameter_hints_prefix = " ",
+      other_hints_prefix = " ",
+    },
   },
   server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<Leader>ha", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
     standalone = false,
     settings = {
       ["rust-analyzer"] = {
