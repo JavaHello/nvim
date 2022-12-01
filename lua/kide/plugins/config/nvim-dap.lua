fd6c570fa6d0098597f702cd56b672aed99a3a9e
luokai <kaiv2@outlook.com>
2022-12-01 23:08:24 +0800

    dapui

--- lua/kide/plugins/config/nvim-dap.lua
+++ lua/kide/plugins/config/nvim-dap.lua
@@ -7,6 +7,7 @@ local dap = require("dap")
 -- }
 local dapui = require("dapui")
 dapui.setup({
+  icons = { expanded = "", collapsed = "", current_frame = "" },
   layouts = {
     {
       elements = {
@@ -23,7 +24,7 @@ dapui.setup({
         "repl",
         -- 'console',
       },
-      size = 12,
+      size = 0.25,
       position = "bottom",
     },
   },
