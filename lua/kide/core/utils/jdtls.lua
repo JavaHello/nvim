ea86dc9a973d836e48877bac657e28eaceb52e85
luokai <kaiv2@outlook.com>
2022-12-11 10:52:18 +0800

    clean jdtls

---
+++ lua/kide/core/utils/jdtls.lua
@@ -0,0 +1,32 @@
+local M = {}
+
+local function delete_file(hf)
+  local files = vim.fn.system("fd -I -H " .. hf .. "$")
+  if files and files ~= "" then
+    for file in string.gmatch(files, "[^\r\n]+") do
+      vim.fn.system("rm -rf " .. file)
+    end
+    return files
+  end
+end
+
+-- fd -I -H settings$ | xargs rm -rf
+-- fd -I -H classpath$ | xargs rm -rf
+-- fd -I -H project$ | xargs rm -rf
+-- fd -I -H factorypath$ | xargs rm -rf
+local function clean_jdtls()
+  local del_files = delete_file("settings") or ""
+  del_files = del_files .. (delete_file("classpath") or "")
+  del_files = del_files .. (delete_file("project") or "")
+  del_files = del_files .. (delete_file("factorypath") or "")
+  vim.notify("delete: \n" .. del_files, vim.log.levels.INFO)
+end
+
+M.setup = function()
+  vim.api.nvim_create_user_command("CleanJdtls", function()
+    clean_jdtls()
+  end, {
+    nargs = 0,
+  })
+end
+return M
