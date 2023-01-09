432b73be096655638862765c31703da508212688
luokai <kaiv2@outlook.com>
2023-01-09 11:49:22 +0800

    ascli-header

--- lua/kide/plugins/config/alpha-nvim.lua
+++ lua/kide/plugins/config/alpha-nvim.lua
@@ -1,20 +1,11 @@
 local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
--- local ascli = require("kide.plugins.config.ascli-header")
+local ascli = require("kide.plugins.config.ascli-header")
 
 dashboard.leader = "\\"
 -- Set header
 -- dashboard.section.header.val = ascli[math.random(0, #ascli)]
-dashboard.section.header.val = {
-  "                                                     ",
-  "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-  "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-  "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-  "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-  "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-  "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-  "                                                     ",
-}
+dashboard.section.header.val = ascli[33]
 local opt = { noremap = true, silent = true }
 -- Set menu
 dashboard.section.buttons.val = {
