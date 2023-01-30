ed0bb972e1a5eba277deef5b131a3fd5ba50236d
luokai <kaiv2@outlook.com>
2023-01-30 13:04:53 +0800

    dapui

--- lua/kide/plugins/config/nvim-dap.lua
+++ lua/kide/plugins/config/nvim-dap.lua
@@ -8,39 +8,19 @@ local dap = require("dap")
 local dapui = require("dapui")
 dapui.setup({
   icons = { expanded = "", collapsed = "", current_frame = "" },
-  layouts = {
-    {
-      elements = {
-        "scopes",
-        "breakpoints",
-        "stacks",
-        "watches",
-      },
-      size = 40,
-      position = "left",
-    },
-    {
-      elements = {
-        "repl",
-        -- 'console',
-      },
-      size = 0.25,
-      position = "bottom",
-    },
-  },
 })
 
-dap.defaults.fallback.terminal_win_cmd = "belowright 12new | set filetype=dap-terminal"
+-- dap.defaults.fallback.terminal_win_cmd = "belowright 12new | set filetype=dap-terminal"
 
--- dap.listeners.after.event_initialized["dapui_config"] = function()
---   dapui.open()
--- end
--- dap.listeners.before.event_terminated["dapui_config"] = function()
---   dapui.close()
--- end
--- dap.listeners.before.event_exited["dapui_config"] = function()
---   dapui.close()
--- end
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
 
 -- vim.fn.sign_define('DapBreakpoint', { text = '🔴', texthl = '', linehl = '', numhl = '' })
 vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "Debug", linehl = "", numhl = "" })
