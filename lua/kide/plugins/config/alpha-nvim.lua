5d7dd6e012bc16f587f1b6f172e47f5f72c592dc
luokai <kaiv2@outlook.com>
2023-03-07 14:41:09 +0800

    查看全路径

--- lua/kide/plugins/config/alpha-nvim.lua
+++ lua/kide/plugins/config/alpha-nvim.lua
@@ -17,7 +17,7 @@ local opt = { noremap = true, silent = true }
 dashboard.section.buttons.val = {
   dashboard.button("<leader>  ff", "  Find File", ":Telescope find_files<CR>", opt),
   dashboard.button("<leader>  fg", "  Find Word  ", ":Telescope live_grep<CR>", opt),
-  dashboard.button("<leader>  fp", "  Recent Projects", ":Telescope project<CR>", opt),
+  dashboard.button("<leader>  fp", "  Recent Projects", ":lua require'telescope'.extensions.project.project{ display_type = 'full' }<CR>", opt),
   dashboard.button("<leader>  fo", "  Recent File", ":Telescope oldfiles<CR>", opt),
   dashboard.button("<leader>  ns", "  Settings", ":e $MYVIMRC | :cd %:p:h <CR>", opt),
   dashboard.button("<leader>  q ", "  Quit NVIM", ":qa<CR>", opt),
