713c5f03d0d9ce31f0328fe53f27b0f1d478de9f
luokai <kaiv2@outlook.com>
2023-04-25 18:09:26 +0800

    lemminx

--- lua/kide/lsp/lemminx.lua
+++ lua/kide/lsp/lemminx.lua
@@ -1,1 +1,24 @@
-return {}
+local M = {}
+local lemminx_home = os.getenv("LEMMINX_HOME")
+if lemminx_home then
+  M.setup = function()
+    local lspconfig = require("lspconfig")
+    local utils = require("kide.core.utils")
+
+    local lemminx_jars = {}
+    for _, bundle in ipairs(vim.split(vim.fn.glob(lemminx_home .. "/*.jar"), "\n")) do
+      table.insert(lemminx_jars, bundle)
+    end
+    vim.fn.join(lemminx_jars, utils.is_win and ";" or ":")
+    lspconfig["lemminx"].setup({
+      cmd = {
+        "java",
+        "-cp",
+        vim.fn.join(lemminx_jars, ":"),
+        "org.eclipse.lemminx.XMLServerLauncher",
+      },
+    })
+  end
+end
+
+return M
