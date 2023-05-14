1e78648051992b286fbebfc283c62e4d53ee5957
luokai <kaiv2@outlook.com>
2023-05-14 23:20:40 +0800

    color

--- lua/kide/plugins/config/gruvbox.lua
+++ lua/kide/plugins/config/gruvbox.lua
@@ -16,14 +16,24 @@ local overrides = {
 require("gruvbox").setup({
   undercurl = true,
   underline = true,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
+  bold = true,
+  italic = {
+    strings = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
   invert_selection = false,
   invert_signs = false,
   invert_tabline = false,
   invert_intend_guides = false,
-  contrast = "", -- can be "hard" or "soft"
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
   overrides = overrides,
+  dim_inactive = false,
+  transparent_mode = false,
 })
-
 vim.opt.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
