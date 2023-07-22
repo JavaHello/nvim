91ffbb3fede1c3fbea3939c519bbcfa069ff8009
luokai <kaiv2@outlook.com>
2023-07-22 11:30:14 +0800

    incremental_selection

--- lua/kide/plugins/lazy-nvim.lua
+++ lua/kide/plugins/lazy-nvim.lua
@@ -196,6 +196,18 @@ require("lazy").setup({
           disable = {},
           additional_vim_regex_highlighting = false,
         },
+        indent = {
+          enable = true,
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+          },
+        },
         textobjects = {
           move = {
             enable = true,
