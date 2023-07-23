99b662932939d73e1b1c49e4064a6f1545976a52
luokai <kaiv2@outlook.com>
2023-07-23 16:05:39 +0800

    Date

--- lua/kide/core/init.lua
+++ lua/kide/core/init.lua
@@ -5,4 +5,6 @@ vim.schedule(function()
   require("kide.core.utils.maven").setup()
   require("kide.core.utils.jdtls").setup()
   require("kide.core.utils.pandoc").setup()
+
+  vim.api.nvim_create_user_command("Date", "lua print(os.date())", {})
 end)
