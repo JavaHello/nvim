cab76c8ef44ae955f912165694635023bcc908d2
luokai <kaiv2@outlook.com>
2023-07-23 15:59:02 +0800

    flash.nvim

--- lua/kide/plugins/lazy-nvim.lua
+++ lua/kide/plugins/lazy-nvim.lua
@@ -815,15 +815,52 @@ require("lazy").setup({
 
   -- 快速跳转
   {
-    "ggandor/leap.nvim",
-    lazy = true,
-    keys = { "s", "S" },
-    config = function()
-      require("leap").add_default_mappings()
-      require("leap").opts.safe_labels = {}
-      vim.keymap.del({ "x", "o" }, "x")
-      vim.keymap.del({ "x", "o" }, "X")
-    end,
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter_search()
+        end,
+        desc = "Treesitter Search",
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function()
+          require("flash").toggle()
+        end,
+        desc = "Toggle Flash Search",
+      },
+    },
   },
 
   -- 查找替换
