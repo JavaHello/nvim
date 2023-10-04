de8edb70357b018b454469dd14e2823a57d40479
luokai <kaiv2@outlook.com>
2023-10-04 21:00:53 +0800

    godot

---
+++ lua/kide/core/utils/godot.lua
@@ -0,0 +1,20 @@
+local M = {}
+local utils = require("kide.core.utils")
+local pipe = (function()
+  if utils.is_win then
+    return vim.env["TEMP"] .. "\\godot.pipe"
+  else
+    return "/tmp/godot.pipe"
+  end
+end)()
+M.setup = function()
+  -- serverstart([{address}])
+  vim.api.nvim_create_user_command("GododNvimServerStart", function()
+    print("start: " .. pipe)
+    vim.fn.serverstart(pipe)
+  end, {})
+  vim.api.nvim_create_user_command("GododNvimServerStop", function()
+    vim.fn.serverstop(pipe)
+  end, {})
+end
+return M
