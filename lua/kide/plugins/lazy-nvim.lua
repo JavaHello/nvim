f462041e47b57e66c92e6002d4d7cfee09ce3d9f
luokai <kaiv2@outlook.com>
2023-12-30 17:36:39 +0800

    go.nvim

--- lua/kide/plugins/lazy-nvim.lua
+++ lua/kide/plugins/lazy-nvim.lua
@@ -1324,6 +1324,19 @@ require("lazy").setup({
       })
     end,
   },
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+  },
   -- {
   --   "xbase-lab/xbase",
   --   build = "make install",
