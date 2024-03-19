a731e549a018ce1ea941f07772d66c85c052e343
luokai <kaiv2@outlook.com>
2024-03-19 14:05:14 +0800

    deps: nvim-neotest/nvim-nio

--- lua/kide/plugins/lazy-nvim.lua
+++ lua/kide/plugins/lazy-nvim.lua
@@ -1409,6 +1409,7 @@ require("lazy").setup({
     "nvim-neotest/neotest",
     lazy = true,
     dependencies = {
+      "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
       "antoinemadec/FixCursorHold.nvim",
       "nvim-treesitter/nvim-treesitter",
