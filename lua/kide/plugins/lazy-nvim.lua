239b08f84358b42f01cf0ca65685c1f4eb7a0c65
luokai <kaiv2@outlook.com>
2024-03-25 10:34:21 +0800

    修复 rest.nvim 依赖问题

--- lua/kide/plugins/lazy-nvim.lua
+++ lua/kide/plugins/lazy-nvim.lua
@@ -469,6 +469,24 @@ require("lazy").setup({
       require("kide.plugins.config.gitsigns-nvim")
     end,
   },
+  {
+    "SuperBo/fugit2.nvim",
+    opts = {},
+    enabled = false,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "nvim-lua/plenary.nvim",
+      {
+        "chrisgrieser/nvim-tinygit", -- optional: for Github PR view
+        dependencies = { "stevearc/dressing.nvim" },
+      },
+    },
+    cmd = { "Fugit2", "Fugit2Graph" },
+    keys = {
+      { "<leader>F", mode = "n", "<cmd>Fugit2<cr>" },
+    },
+  },
 
   -- 浮动窗口插件
   {
@@ -821,47 +839,18 @@ require("lazy").setup({
     "simrat39/rust-tools.nvim",
     lazy = true,
   },
-
   {
-    "NTBBloodbath/rest.nvim",
+    "vhyrro/luarocks.nvim",
+    priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+    config = true,
+  },
+  {
+    "rest-nvim/rest.nvim",
     dependencies = { "luarocks.nvim" },
     lazy = true,
     ft = "http",
-    init = function()
-      local group = vim.api.nvim_create_augroup("kide_jdtls_rest_http", { clear = true })
-      vim.api.nvim_create_autocmd({ "FileType" }, {
-        group = group,
-        pattern = { "http" },
-        callback = function(o)
-          vim.api.nvim_buf_create_user_command(o.buf, "Http", ":lua require'rest-nvim'.run()", { nargs = 0 })
-          vim.api.nvim_buf_create_user_command(o.buf, "HttpCurl", ":lua require'rest-nvim'.run(true)", { nargs = 0 })
-          vim.api.nvim_buf_create_user_command(o.buf, "HttpLast", ":lua require'rest-nvim'.last()", { nargs = 0 })
-        end,
-      })
-    end,
     config = function()
-      require("rest-nvim").setup({
-        -- Open request results in a horizontal split
-        result_split_horizontal = false,
-        -- Skip SSL verification, useful for unknown certificates
-        skip_ssl_verification = false,
-        -- Highlight request on run
-        highlight = {
-          enabled = true,
-          timeout = 150,
-        },
-        result = {
-          -- toggle showing URL, HTTP info, headers at top the of result window
-          show_url = true,
-          show_http_info = true,
-          show_headers = true,
-        },
-        -- Jump to request line on run
-        jump_to_request = false,
-        env_file = ".env",
-        custom_dynamic_variables = {},
-        yank_dry_run = true,
-      })
+      require("rest-nvim").setup({})
     end,
   },
 
