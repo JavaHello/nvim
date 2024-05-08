69b81323ab3495c4abe66f0819504fa9c5ff47c7
luokai <kaiv2@outlook.com>
2024-05-08 19:51:07 +0800

    py buf key map

--- lua/kide/lsp/pyright.lua
+++ lua/kide/lsp/pyright.lua
@@ -6,9 +6,9 @@ M.setup = function(opts)
   local config = vim.tbl_deep_extend("keep", {
     on_attach = function(client, bufnr)
       local dap_py = require "dap-python"
-      vim.keymap.set("n", "<leader>dc", dap_py.test_class, { desc = "Dap Test Class" })
-      vim.keymap.set("n", "<leader>dm", dap_py.test_method, { desc = "Dap Test Method" })
-      vim.keymap.set("v", "<leader>ds", dap_py.debug_selection, { desc = "Dap Debug Selection" })
+      vim.keymap.set("n", "<leader>dc", dap_py.test_class, { desc = "Dap Test Class", buffer = bufnr })
+      vim.keymap.set("n", "<leader>dm", dap_py.test_method, { desc = "Dap Test Method", buffer = bufnr })
+      vim.keymap.set("v", "<leader>ds", dap_py.debug_selection, { desc = "Dap Debug Selection", buffer = bufnr })
       on_attach(client, bufnr)
     end,
   }, opts)
