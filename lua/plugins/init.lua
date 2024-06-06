3313b234d05edf347ec4d2d23773752c6f39524c
luokai <kaiv2@outlook.com>
2024-06-06 21:04:43 +0800

    使用 DeepseekChat

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -301,7 +301,27 @@ return {
       "GpChatToggle",
     },
     opts = {
+      openai_api_key = vim.env["OPENAI_API_KEY"],
+      cmd_prefix = "Gp",
       openai_api_endpoint = vim.env["OPENAI_API_ENDPOINT"],
+      chat_topic_gen_model = "deepseek-chat",
+      agents = {
+        {
+          name = "DeepseekChat",
+          chat = true,
+          command = false,
+          model = { model = "deepseek-chat", temperature = 1.0, top_p = 1 },
+          system_prompt = "You are a general AI assistant.\n\n"
+            .. "The user provided the additional info about how they would like you to respond:\n\n"
+            .. "- If you're unsure don't guess and say you don't know instead.\n"
+            .. "- Ask question if you need clarification to provide better answer.\n"
+            .. "- Think deeply and carefully from first principles step by step.\n"
+            .. "- Zoom out first to see the big picture and then zoom in to details.\n"
+            .. "- Use Socratic method to improve your thinking and coding skills.\n"
+            .. "- Don't elide any code from your output if the answer requires coding.\n"
+            .. "- Take a deep breath; You've got this!\n",
+        },
+      },
     },
     config = function(_, opts)
       require("gp").setup(opts)
