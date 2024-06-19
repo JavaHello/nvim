f2b52619eff459dc5f4d7c375aa91cd92167c935
luokai <kaiv2@outlook.com>
2024-06-19 14:14:26 +0800

    clearjumps

--- init.lua
+++ init.lua
@@ -1,6 +1,13 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
 
+vim.cmd [[
+  augroup kide_clearjumps
+    autocmd!
+    autocmd VimEnter * :clearjumps
+  augroup END
+]]
+
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
