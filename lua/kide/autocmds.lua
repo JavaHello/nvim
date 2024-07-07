7b3fcad9cdcb084e5a27dd1e29aaba21e568c1ec
luokai <kaiv2@outlook.com>
2024-07-07 10:34:58 +0800

    Restore cursor position

--- lua/kide/autocmds.lua
+++ lua/kide/autocmds.lua
@@ -10,6 +10,22 @@ autocmd({ "TextYankPost" }, {
   end,
 })
 
+-- https://nvchad.com/docs/recipes
+autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local line = vim.fn.line "'\""
+    if
+      line > 1
+      and line <= vim.fn.line "$"
+      and vim.bo.filetype ~= "commit"
+      and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+    then
+      vim.cmd 'normal! g`"'
+    end
+  end,
+})
+
 -- close some filetypes with <q>
 autocmd("FileType", {
   group = augroup "close_with_q",
