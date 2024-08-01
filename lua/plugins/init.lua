6e932a3c2bac5c78ccbe78499a2fb6d0fec85f35
luokai <kaiv2@outlook.com>
2024-08-01 23:49:06 +0800

    rustaceanvim version 5

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -360,7 +360,7 @@ return {
   -- rust
   {
     "mrcjkb/rustaceanvim",
-    version = "^4", -- Recommended
+    version = "^5", -- Recommended
     lazy = false, -- This plugin is already lazy
     config = function()
       vim.g.rustaceanvim = {
