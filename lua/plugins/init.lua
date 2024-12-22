504fd3081600e12d93862a1032e1f199dc3109c5
luokai <kaiv2@outlook.com>
2024-12-22 11:22:50 +0800

    telescope-fzy

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -281,6 +281,13 @@ return {
       require("telescope").load_extension "ui-select"
     end,
   },
+  {
+    "nvim-telescope/telescope-fzy-native.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("telescope").load_extension "fzy_native"
+    end,
+  },
 
   -- Note
   {
