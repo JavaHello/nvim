b2edefeb07391ba9f999e2ba0c1f7093e93a154d
luokai <kaiv2@outlook.com>
2025-01-03 21:48:43 +0800

    spring-boot config

--- ftplugin/jproperties.lua
+++ ftplugin/jproperties.lua
@@ -1,1 +1,5 @@
-require("spring_boot.launch").start(require("kide.lsp.spring-boot").config)
+if vim.env["SPRING_BOOT_NVIM_ENABLE"] == "Y" then
+  if require("spring_boot.util").is_application_properties_buf(0) then
+    require("spring_boot.launch").start(require("kide.lsp.spring-boot").config)
+  end
+end
