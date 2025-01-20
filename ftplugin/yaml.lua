25f9461a48ac777cd051e41561a0bbd9f251e2be
luokai <kaiv2@outlook.com>
2025-01-20 21:21:41 +0800

    refactor: replace custom LSP launch with vim.lsp.start in jproperties and yaml ftplugins

--- ftplugin/yaml.lua
+++ ftplugin/yaml.lua
@@ -1,5 +1,5 @@
 if vim.env["SPRING_BOOT_NVIM_ENABLE"] == "Y" then
   if require("spring_boot.util").is_application_yml_buf(0) then
-    require("spring_boot.launch").start(require("kide.lsp.spring-boot").config)
+    vim.lsp.start(require("kide.lsp.spring-boot").config)
   end
 end
