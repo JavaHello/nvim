f604328e34bdd3c78156b2c11b5fd65ee83a2b44
luokai <kaiv2@outlook.com>
2025-04-05 11:18:57 +0800

    neovide_opacity

--- init.lua
+++ init.lua
@@ -49,7 +49,7 @@ if vim.g.neovide then
     return string.format("%x", math.floor(255 * (vim.g.transparency or 0.8)))
   end
   -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-  vim.g.neovide_transparency = 1.0
+  vim.g.neovide_opacity = 1.0
   vim.g.neovide_background_color = "#282828" .. alpha()
 
   vim.g.neovide_floating_blur_amount_x = 2.0
