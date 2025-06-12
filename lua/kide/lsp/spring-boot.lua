5aca7a079d44561d97deb8858aae12bba2634b00
luokai <kaiv2@outlook.com>
2025-06-12 19:20:59 +0800

    fix(lsp): improve spring-boot lsp path handling and initialization

--- lua/kide/lsp/spring-boot.lua
+++ lua/kide/lsp/spring-boot.lua
@@ -1,9 +1,5 @@
 local M = {}
 local me = require("kide.melspconfig")
-local jars = require("spring_boot").java_extensions()
-if jars == nil or #jars <= 0 then
-  return M
-end
 local function ls_path()
   local path = vim.env["JDTLS_SPRING_TOOLS_PATH"]
   if path == nil or path == "" then
@@ -11,8 +7,12 @@ local function ls_path()
   end
   return require("spring_boot").get_boot_ls(path .. "/language-server")
 end
+local lspath = ls_path()
+if lspath == nil then
+  return M
+end
 M.config = require("spring_boot.launch").update_ls_config(require("spring_boot").setup({
-  ls_path = ls_path(),
+  ls_path = lspath,
   server = {
     on_attach = me.on_attach,
     on_init = function(client, ctx)
