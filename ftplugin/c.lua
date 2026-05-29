d1a8e06271f8ff0863b8561de17b77266ee78db2
luokai <kaiv2@outlook.com>
2026-05-29 22:53:22 +0800

    feat: add cppcheck linting integration

--- ftplugin/c.lua
+++ ftplugin/c.lua
@@ -5,3 +5,7 @@ if require("kide.bigfile").bigfile(vim.api.nvim_get_current_buf()) then
   return
 end
 require("kide.lsp.clangd").start()
+
+if vim.fn.executable("cppcheck") == 1 then
+  require("lint").try_lint("cppcheck")
+end
